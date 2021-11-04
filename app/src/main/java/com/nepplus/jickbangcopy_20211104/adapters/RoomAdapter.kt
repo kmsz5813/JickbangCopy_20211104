fe06a37547312ebf29678fe34af706050729ac16
김민성 <kmsz5813@naver.com>
2021-11-04 12:17:00 +0900

    부모에게 넘겨주기위한 파라미터들을, RoomAdaper의 생성자를 통해서 받아오게

--- app/src/main/java/com/nepplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.jickbangcopy_20211104.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.nepplus.jickbangcopy_20211104.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomData>() {
+class RoomAdapter(
+    val mContext: Context,
+    val redId: Int,
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>() {
 }
