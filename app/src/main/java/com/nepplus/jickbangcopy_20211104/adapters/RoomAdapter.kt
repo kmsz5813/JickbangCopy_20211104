5bbbaf5fe010e1b3af046b2a13c8696011f42ba2
김민성 <kmsz5813@naver.com>
2021-11-04 12:20:45 +0900

    mInflater 생성, getView 오버라이딩

--- app/src/main/java/com/nepplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20211104/adapters/RoomAdapter.kt
@@ -1,6 +1,9 @@
 package com.nepplus.jickbangcopy_20211104.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.nepplus.jickbangcopy_20211104.datas.RoomData
 
@@ -8,4 +11,11 @@ class RoomAdapter(
     val mContext: Context,
     val redId: Int,
     val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>( mContext, redId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+    }
+
 }
